cf22f67b40b1a922cd422a25c975e3d8f739133c
qgymib <qgymib@gmail.com>
2020-04-11 14:02:04 +0800

    添加注释

--- include/EAF/plugin/plugin.h
+++ include/EAF/plugin/plugin.h
@@ -8,7 +8,14 @@ extern "C" {
 
 #define EAF_PLUGIN_SERVICE_MSG		0XF0000000
 
+/**
+* EAF不会主动加载插件系统。若想要加载插件，则需要在setup之后、load之前调用此函数
+*/
 void eaf_plugin_load(void);
+
+/**
+* 卸载插件。需要在cleanup阶段之后调用此函数
+*/
 void eaf_plugin_unload(void);
 
 #ifdef __cplusplus
