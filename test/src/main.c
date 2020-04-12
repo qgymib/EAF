3c0bc4194c97a04e6cdd2fb37f4c9ac1085b4e2a
qgymib <qgymib@gmail.com>
2020-04-12 23:07:18 +0800

    fix: CRT does not show memory leak

--- test/src/main.c
+++ test/src/main.c
@@ -2,7 +2,16 @@
 
 int main(int argc, char* argv[])
 {
-	malloc(1);
+#if defined(_MSC_VER)
+	// Send all reports to STDOUT
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDOUT);
+	_CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDOUT);
+#endif
+
 	int ret = test_run_tests(argc, argv);
 
 #if defined(_MSC_VER)
