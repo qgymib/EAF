2aacac46df39ebc47d83698885c6467a4a179339
qgymib <qgymib@gmail.com>
2020-04-20 21:49:09 +0800

    ci: remove clang compiler

--- ci/upload_coverage.sh
+++ ci/upload_coverage.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# coverage should only valid on master branch
-if [ "$TRAVIS_BRANCH" != "master" ]; then
-	exit 0
-fi
-
-if [ "$CC" = "gcc" ]; then
-	bash <(curl -s https://codecov.io/bash)
-fi
+bash <(curl -s https://codecov.io/bash)
