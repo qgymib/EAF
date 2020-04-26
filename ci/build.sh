79a556cd923db5f523c9aaa0cd87153059e7e96b
qgymib <qgymib@gmail.com>
2020-04-26 16:59:16 +0000

    ci should stop on error

--- ci/build.sh
+++ ci/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # run test and generate coverage
-(rm -rf build && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_COVERAGE=1 .. && make coverage)
+(rm -rf build && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_COVERAGE=1 .. && make coverage) || exit 1
 
 # doxygen
-(doxygen Doxyfile)
+(doxygen Doxyfile) || exit 1
