a9b68bd312b1ba4f8b9cf0968f7907c459b20a63
qgymib <qgymib@gmail.com>
2020-05-26 09:33:48 +0800

    simplify test

--- test/src/powerpack/hash.c
+++ test/src/powerpack/hash.c
@@ -16,9 +16,6 @@ TEST_F(powerpack_hash, bkdr_32)
 	const char* str_b = __FUNCTION__;
 	uint32_t seed = __LINE__;
 
-	uint32_t hval_a = eaf_hash32_bkdr(str_a, strlen(str_a), seed);
-	uint32_t hval_b = eaf_hash32_bkdr(str_b, strlen(str_b), seed);
-
 	ASSERT_EQ_STR(str_a, str_b);
-	ASSERT_EQ_U32(hval_a, hval_b);
+	ASSERT_EQ_U32(eaf_hash32_bkdr(str_a, strlen(str_a), seed), eaf_hash32_bkdr(str_b, strlen(str_b), seed));
 }
