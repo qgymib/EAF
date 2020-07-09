8327b413acc177ef9a4168112ef58321d1b0e762
qgymib <qgymib@gmail.com>
2020-07-09 16:37:33 +0800

    [skip ci]message is able to carry user defined token

--- test/src/eaf/message.c
+++ test/src/eaf/message.c
@@ -9,5 +9,25 @@ TEST(eaf_msg, receipt)
 	eaf_msg_set_receipt(msg, eaf_errno_invalid);
 	ASSERT_EQ_D32(eaf_msg_get_receipt(msg), eaf_errno_invalid);
 
+	eaf_msg_set_receipt(msg, eaf_errno_success);
+	ASSERT_EQ_D32(eaf_msg_get_receipt(msg), eaf_errno_success);
+
+	eaf_msg_dec_ref(msg);
+}
+
+TEST(eaf_msg, token)
+{
+	eaf_msg_t* msg = eaf_msg_create_req(0, 0, NULL);
+	ASSERT_NE_PTR(msg, NULL);
+
+	eaf_msg_set_token(msg, 1000);
+	ASSERT_EQ_D32(eaf_msg_get_token(msg), 1000);
+
+	eaf_msg_set_token(msg, -1000);
+	ASSERT_EQ_D32(eaf_msg_get_token(msg), -1000);
+
+	eaf_msg_set_token(msg, 0);
+	ASSERT_EQ_D32(eaf_msg_get_token(msg), 0);
+
 	eaf_msg_dec_ref(msg);
 }
