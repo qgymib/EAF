344dccb245cb5ed02dcfb7537c692d28b65507c0
qgymib <qgymib@gmail.com>
2020-07-11 22:06:44 +0800

    timer: add helper macro

--- powerpack/include/eaf/powerpack/timer.h
+++ powerpack/include/eaf/powerpack/timer.h
@@ -23,27 +23,52 @@ extern "C" {
 /**
  * @ingroup PowerPack-Timer
  * @defgroup PowerPack-Timer-Delay Delay
+ *
+ * @ note You can use #EAF_TIMER_DELAY helper macro to quickly use this message
  * @{
  */
 
+/**
+ * @brief Request ID for timer message: delay
+ */
 #define EAF_TIMER_MSG_DELAY_REQ		(EAF_TIMER_ID + 0x0001)
+/**
+ * @brief Request for timer message: delay
+ */
 typedef struct eaf_timer_delay_req
 {
-	uint32_t	msec;
+	uint32_t	msec;	/**< Timeout in milliseconds */
 }eaf_timer_delay_req_t;
+/**
+ * @brief Response ID for timer message: delay
+ */
 #define EAF_TIMER_MSG_DELAY_RSP		EAF_TIMER_MSG_DELAY_REQ
+ /**
+  * @brief Response for timer message: delay
+  */
 typedef struct eaf_timer_delay_rsp
 {
-	int32_t		ret;
+	int32_t		ret;	/**< Result */
 }eaf_timer_delay_rsp_t;
 
 /**
- * @brief A helper function for create timer delay request
- * @param[in] msec		Delay timeout
- * @param[in] handle	Response handler
- * @return				Request message with reference count set to 1.
+ * @brief A helper macro to quickly send delay message
+ * @param[out] ret		An integer to store result
+ * @param[in] from		Who send this message
+ * @param[in] rsp_fn	The function for handle response
+ * @param[in] timeout	Timeout in milliseconds
  */
-int eaf_timer_delay(uint32_t msec, eaf_msg_handle_fn handle);
+#define EAF_TIMER_DELAY(ret, from, rsp_fn, timeout)	\
+	do {\
+		eaf_msg_t* req = eaf_msg_create_req(EAF_TIMER_MSG_DELAY_REQ,\
+			sizeof(eaf_timer_delay_req_t), rsp_fn);\
+		if (req == NULL) {\
+			ret = eaf_errno_memory;\
+		}\
+		((eaf_timer_delay_req_t*)eaf_msg_get_data(req, NULL))->msec = timeout;\
+		ret = eaf_send_req(from, EAF_TIMER_ID, req);\
+		eaf_msg_dec_ref(req);\
+	} EAF_MSVC_WARNING_GUARD(4127, while (0))
 
 /**
  * @}
