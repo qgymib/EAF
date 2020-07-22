ba24e5fad7de7219c52b0dcee6f2a0221720c4cd
qgymib <qgymib@gmail.com>
2020-07-22 21:43:42 +0800

    disable doxygen for TravisCI

--- ci/build.sh
+++ ci/build.sh
@@ -2,6 +2,3 @@
 
 # run test and generate coverage
 (rm -rf build && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_COVERAGE=1 .. && make coverage) || exit 1
-
-# doxygen
-(doxygen Doxyfile) || exit 1
