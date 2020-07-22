0c17dba42f1766086639895668451747a53f3bc6
qgymib <qgymib@gmail.com>
2020-07-22 10:32:24 +0800

    monitor: add cpu time

--- powerpack/src/powerpack/monitor.c
+++ powerpack/src/powerpack/monitor.c
@@ -21,57 +21,83 @@
 	} while (0)
 #endif
 
-static int _monitor_on_cmp_dataflow_record(const eaf_map_node_t* key1, const eaf_map_node_t* key2, void* arg);
-static int _monitor_on_cmp_service_record_group(const eaf_map_node_t* key1, const eaf_map_node_t* key2, void* arg);
-static int _monitor_on_cmp_service_record_split(const eaf_map_node_t* key1, const eaf_map_node_t* key2, void* arg);
+static int _monitor_cmp_dataflow_record(const eaf_map_node_t* key1, const eaf_map_node_t* key2, void* arg);
+static int _monitor_cmp_service_record_group(const eaf_map_node_t* key1, const eaf_map_node_t* key2, void* arg);
+static int _monitor_cmp_service_record_split(const eaf_map_node_t* key1, const eaf_map_node_t* key2, void* arg);
 static void _monitor_on_load_before(void);
 static void _monitor_on_message_send_after(uint32_t from, uint32_t to, eaf_msg_t* msg, int ret);
+static int _monitor_on_message_handle_before(uint32_t from, uint32_t to, eaf_msg_t* msg);
 static void _monitor_on_message_handle_after(uint32_t from, uint32_t to, eaf_msg_t* msg);
 static void _monitor_on_exit_before(void);
 
 typedef struct monitor_dataflow_record
 {
-	eaf_map_node_t				node;			/**< node for eaf_monitor_ctx_t::dataflow::record */
+	eaf_map_node_t					node;			/**< node for eaf_monitor_ctx_t::dataflow::record */
 	struct
 	{
-		uint32_t				from;			/** Who send this message */
-		uint32_t				to;				/** Who will receive this message */
-		size_t					count;			/** Message count */
+		uint32_t					from;			/** Who send this message */
+		uint32_t					to;				/** Who will receive this message */
+		size_t						count;			/** Message count */
 	}data;
 }monitor_dataflow_record_t;
 
 typedef struct monitor_service_record
 {
-	eaf_map_node_t				node_group;		/**< node for eaf_monitor_ctx_t::serivce::record_group */
-	eaf_map_node_t				node_split;		/**< node for eaf_monitor_ctx_t::serivce::record_split */
+	eaf_map_node_t					node_group;		/**< node for eaf_monitor_ctx_t::serivce::record_group */
+	eaf_map_node_t					node_split;		/**< node for eaf_monitor_ctx_t::serivce::record_split */
 
-	uv_mutex_t					objlock;		/**< Object lock */
+	uv_mutex_t						objlock;		/**< Object lock */
 
 	struct
 	{
-		uint32_t				gid;			/**< Group ID */
-		uint32_t				sid;			/**< Service ID. It must be unique */
-		eaf_service_local_t*	sls;			/**< Service Local Storage */
+		uint32_t					gid;			/**< Group ID */
+		uint32_t					sid;			/**< Service ID. It must be unique */
+		eaf_service_local_t*		sls;			/**< Service Local Storage */
 	}data;
 
 	struct
 	{
-		uint32_t				cnt_send;		/** The number of message send */
-		uint32_t				cnt_recv;		/** The number of message recv */
+		uint32_t					flush_send;		/**< The number of message send */
+		uint32_t					flush_recv;		/**< The number of message recv */
+		uint64_t					flush_use_time;	/**< Use time in nanoseconds */
+
+		uint64_t					total_send;		/**< The total number of message send */
+		uint64_t					total_recv;		/**< The total number of message recv */
 	}counter;
+
+	struct
+	{
+		uint64_t					use_time_start;	/**< The start point of use time */
+	}temp;
 }monitor_service_record_t;
+
+typedef struct monitor_group_record
+{
+	uv_mutex_t						objlock;		/**< Object lock */
+
+	struct
+	{
+		uint64_t					flush_use_time;	/**< Use time in nanoseconds */
+	}counter;
+}monitor_group_record_t;
 
 typedef struct eaf_monitor_ctx
 {
 	struct
 	{
-		eaf_map_t				record_group;	/**< Group by GID, search by `gid' and `sid' */
-		eaf_map_t				record_split;	/**< Independent record, search by `sid' */
+		eaf_map_t					record_group;	/**< Group by GID, search by `gid' and `sid' */
+		eaf_map_t					record_split;	/**< Independent record, search by `sid' */
 	}serivce;
 
 	struct
 	{
-		eaf_map_t				record;			/**< A insert-only table for record message flow */
+		size_t						size;			/**< Array size */
+		monitor_group_record_t*		table;			/** Group record */
+	}group;
+
+	struct
+	{
+		eaf_map_t					record;			/**< A insert-only table for record message flow */
 	}dataflow;
 }eaf_monitor_ctx_t;
 
@@ -79,27 +105,31 @@ typedef struct eaf_monitor_ctx2
 {
 	struct
 	{
-		uv_timer_t				timer;
+		uv_timer_t					timer;			/**< Global refresh timer */
 		struct
 		{
-			unsigned			running : 1;
+			unsigned				running : 1;	/**< Timer is running */
 		}mask;
+		uv_mutex_t					objlock;		/**< Object lock */
 	}refresh;
 
 	struct
 	{
-		uv_rwlock_t				rwlock;
+		uv_rwlock_t					rwlock;			/**< rwlock for eaf_monitor_ctx_t::dataflow::record */
 	}dataflow;
 }eaf_monitor_ctx2_t;
 
 static eaf_monitor_ctx2_t g_eaf_monitor_ctx2;
 static eaf_monitor_ctx_t g_eaf_monitor_ctx = {
 	{
-		EAF_MAP_INITIALIZER(_monitor_on_cmp_service_record_group, NULL),
-		EAF_MAP_INITIALIZER(_monitor_on_cmp_service_record_split, NULL),
+		EAF_MAP_INITIALIZER(_monitor_cmp_service_record_group, NULL),
+		EAF_MAP_INITIALIZER(_monitor_cmp_service_record_split, NULL),
 	},																		// .serivce
 	{
-		EAF_MAP_INITIALIZER(_monitor_on_cmp_dataflow_record, NULL),
+		0, NULL																// .group
+	},
+	{
+		EAF_MAP_INITIALIZER(_monitor_cmp_dataflow_record, NULL),
 	},																		// .dataflow
 };
 
@@ -113,7 +143,7 @@ static eaf_powerpack_hook_t g_pp_hook = {
 		NULL,								// .on_service_yield
 		NULL,								// .on_service_resume
 		NULL,								// .on_service_register
-		NULL,								// .on_message_send_before
+		_monitor_on_message_handle_before,	// .on_message_send_before
 		_monitor_on_message_send_after,		// .on_message_send_after
 		NULL,								// .on_message_handle_before
 		_monitor_on_message_handle_after,	// .on_message_handle_after
@@ -124,7 +154,7 @@ static eaf_powerpack_hook_t g_pp_hook = {
 	}
 };
 
-static int _monitor_on_cmp_service_record_group(const eaf_map_node_t* key1, const eaf_map_node_t* key2, void* arg)
+static int _monitor_cmp_service_record_group(const eaf_map_node_t* key1, const eaf_map_node_t* key2, void* arg)
 {
 	EAF_SUPPRESS_UNUSED_VARIABLE(arg);
 
@@ -152,7 +182,7 @@ static int _monitor_on_cmp_service_record_group(const eaf_map_node_t* key1, cons
 	return 0;
 }
 
-static int _monitor_on_cmp_service_record_split(const eaf_map_node_t* key1, const eaf_map_node_t* key2, void* arg)
+static int _monitor_cmp_service_record_split(const eaf_map_node_t* key1, const eaf_map_node_t* key2, void* arg)
 {
 	EAF_SUPPRESS_UNUSED_VARIABLE(arg);
 
@@ -166,7 +196,7 @@ static int _monitor_on_cmp_service_record_split(const eaf_map_node_t* key1, cons
 	return rec_1->data.sid < rec_2->data.sid ? -1 : 1;
 }
 
-static int _monitor_on_cmp_dataflow_record(const eaf_map_node_t* key1, const eaf_map_node_t* key2, void* arg)
+static int _monitor_cmp_dataflow_record(const eaf_map_node_t* key1, const eaf_map_node_t* key2, void* arg)
 {
 	EAF_SUPPRESS_UNUSED_VARIABLE(arg);
 
@@ -203,81 +233,18 @@ static monitor_service_record_t* _monitor_find_serivce(uint32_t sid)
 	return it != NULL ? EAF_CONTAINER_OF(it, monitor_service_record_t, node_split) : NULL;
 }
 
-static void _monitor_update_counter_recv(uint32_t sid)
-{
-	monitor_service_record_t* record = _monitor_find_serivce(sid);
-
-	uv_mutex_lock(&record->objlock);
-	record->counter.cnt_recv++;
-	uv_mutex_unlock(&record->objlock);
-}
-
 static void _monitor_update_counter_send(uint32_t sid)
 {
 	monitor_service_record_t* record = _monitor_find_serivce(sid);
 
 	uv_mutex_lock(&record->objlock);
-	record->counter.cnt_send++;
+	{
+		record->counter.flush_send++;
+	}
 	uv_mutex_unlock(&record->objlock);
-}
 
-static void _monitor_on_message_handle_after(uint32_t from, uint32_t to, eaf_msg_t* msg)
-{
-	EAF_SUPPRESS_UNUSED_VARIABLE(from, msg);
-
-	_monitor_update_counter_recv(to);
-}
-
-static void _monitor_on_timer_close(uv_handle_t* handle)
-{
-	EAF_SUPPRESS_UNUSED_VARIABLE(handle);
-
-	g_eaf_monitor_ctx2.refresh.mask.running = 0;
-}
-
-static void _monitor_on_exit_before(void)
-{
-	uv_timer_stop(&g_eaf_monitor_ctx2.refresh.timer);
-	uv_close((uv_handle_t*)&g_eaf_monitor_ctx2.refresh.timer, _monitor_on_timer_close);
-
-	while (g_eaf_monitor_ctx2.refresh.mask.running)
-	{
-		eaf_thread_sleep(10);
-	}
-}
-
-static void _monitor_on_load_before(void)
-{
-	uint32_t idx_gls;
-	eaf_group_local_t* gls = eaf_group_begin();
-	for (idx_gls = 0; gls != NULL; gls = eaf_group_next(gls), idx_gls++)
-	{
-		eaf_service_local_t* sls = eaf_service_begin(gls);
-		for (; sls != NULL; sls = eaf_service_next(gls, sls))
-		{
-			monitor_service_record_t* record = malloc(sizeof(*record));
-			assert(record != NULL);
-
-			record->data.gid = idx_gls;
-			record->data.sid = sls->id;
-			record->data.sls = sls;
-			record->counter.cnt_send = 0;
-			record->counter.cnt_recv = 0;
-
-			if (uv_mutex_init(&record->objlock) < 0)
-			{
-				free(record);
-				continue;
-			}
-
-			if (eaf_map_insert(&g_eaf_monitor_ctx.serivce.record_split, &record->node_split) < 0)
-			{
-				free(record);
-				continue;
-			}
-			eaf_map_insert(&g_eaf_monitor_ctx.serivce.record_group, &record->node_group);
-		}
-	}
+	/* no race condition, so it is ok outside of mutex protection */
+	record->counter.total_send++;
 }
 
 static void _monitor_dataflow_insert_new_record(uint32_t from, uint32_t to)
@@ -332,18 +299,17 @@ static void _monitor_update_message_path(uint32_t from, uint32_t to)
 	}
 }
 
-static void _monitor_on_message_send_after(uint32_t from, uint32_t to, eaf_msg_t* msg, int ret)
+static float _monitor_calculate_cpu(uint64_t use, uint64_t total)
 {
-	EAF_SUPPRESS_UNUSED_VARIABLE(msg);
-
-	/* No need to record failure case */
-	if (ret < 0)
+	if (total == 0)
 	{
-		return;
+		return 0.0f;
 	}
 
-	_monitor_update_message_path(from, to);
-	_monitor_update_counter_send(from);
+	float ret = ((float)use / (float)total) * 100;
+	const float gate = 99.9f;
+
+	return ret >= gate ? gate : ret;
 }
 
 static const char* _monitor_state_2_string(eaf_service_state_t state)
@@ -375,10 +341,41 @@ static const char* _monitor_state_2_string(eaf_service_state_t state)
 	return "UNKNOWN";
 }
 
-static void _monitor_on_timer_cb(uv_timer_t* handle)
+static void _monitor_print_tree_nolock(char* buffer, size_t size)
 {
-	EAF_SUPPRESS_UNUSED_VARIABLE(handle);
+	buffer[0] = '\0';
 
+	APPEND_LINE(buffer, size, "[SERVICE]    [STATE]   [RECV]   [SEND] [MSGQ] [TIME]  [CPU%%]\n");
+
+	uint32_t last_gid = (uint32_t)-1;
+	monitor_group_record_t* g_record = NULL;
+
+	eaf_map_node_t* it = eaf_map_begin(&g_eaf_monitor_ctx.serivce.record_group);
+	for (; it != NULL; it = eaf_map_next(it))
+	{
+		monitor_service_record_t* record = EAF_CONTAINER_OF(it, monitor_service_record_t, node_group);
+
+		if (last_gid != record->data.gid)
+		{
+			last_gid = record->data.gid;
+			g_record = &g_eaf_monitor_ctx.group.table[last_gid];
+			APPEND_LINE(buffer, size, "%u\n", (unsigned)record->data.gid);
+		}
+
+		APPEND_LINE(buffer, size, "|-%#010"PRIx32" %-7s %8"PRIu32" %8"PRIu32" %6u %6u    %4.1f\n",
+			record->data.sid,
+			_monitor_state_2_string(record->data.sls->state),
+			record->counter.flush_recv,
+			record->counter.flush_send,
+			(unsigned)eaf_message_queue_size(record->data.sls),
+			(unsigned)(record->counter.flush_use_time / 1000 / 1000),
+			_monitor_calculate_cpu(record->counter.flush_use_time, g_record->counter.flush_use_time));
+	}
+}
+
+static void _monitor_reset_flush_nolock(void)
+{
+	/* reset service counter */
 	eaf_map_node_t* it = eaf_map_begin(&g_eaf_monitor_ctx.serivce.record_group);
 	for (; it != NULL; it = eaf_map_next(it))
 	{
@@ -387,11 +384,162 @@ static void _monitor_on_timer_cb(uv_timer_t* handle)
 		/* Reset counter */
 		uv_mutex_lock(&record->objlock);
 		{
-			record->counter.cnt_send = 0;
-			record->counter.cnt_recv = 0;
+			record->counter.flush_send = 0;
+			record->counter.flush_recv = 0;
+			record->counter.flush_use_time = 0;
 		}
 		uv_mutex_unlock(&record->objlock);
 	}
+
+	/* reset group counter */
+	size_t i;
+	for (i = 0; i < g_eaf_monitor_ctx.group.size; i++)
+	{
+		monitor_group_record_t* record = &g_eaf_monitor_ctx.group.table[i];
+
+		uv_mutex_lock(&record->objlock);
+		{
+			record->counter.flush_use_time = 0;
+		}
+		uv_mutex_unlock(&record->objlock);
+	}
+}
+
+static int _monitor_on_message_handle_before(uint32_t from, uint32_t to, eaf_msg_t* msg)
+{
+	EAF_SUPPRESS_UNUSED_VARIABLE(from, msg);
+
+	monitor_service_record_t* record = _monitor_find_serivce(to);
+	assert(record != NULL);
+
+	record->temp.use_time_start = uv_hrtime();
+	return 0;
+}
+
+static void _monitor_on_message_handle_after(uint32_t from, uint32_t to, eaf_msg_t* msg)
+{
+	EAF_SUPPRESS_UNUSED_VARIABLE(from, msg);
+
+	monitor_service_record_t* record = _monitor_find_serivce(to);
+	assert(record != NULL);
+
+	uint64_t use_time = uv_hrtime() - record->temp.use_time_start;
+
+	uv_mutex_lock(&record->objlock);
+	{
+		record->counter.flush_recv++;
+		record->counter.flush_use_time += use_time;
+	}
+	uv_mutex_unlock(&record->objlock);
+
+	/* no race condition, so it is ok outside of mutex protection */
+	record->counter.total_recv++;
+
+	monitor_group_record_t* g_record = &g_eaf_monitor_ctx.group.table[record->data.gid];
+	uv_mutex_lock(&g_record->objlock);
+	{
+		g_record->counter.flush_use_time += use_time;
+	}
+	uv_mutex_unlock(&g_record->objlock);
+}
+
+static void _monitor_on_uv_timer_close(uv_handle_t* handle)
+{
+	EAF_SUPPRESS_UNUSED_VARIABLE(handle);
+
+	g_eaf_monitor_ctx2.refresh.mask.running = 0;
+}
+
+static void _monitor_on_exit_before(void)
+{
+	uv_timer_stop(&g_eaf_monitor_ctx2.refresh.timer);
+	uv_close((uv_handle_t*)&g_eaf_monitor_ctx2.refresh.timer, _monitor_on_uv_timer_close);
+
+	/* wait until timer stop */
+	while (g_eaf_monitor_ctx2.refresh.mask.running)
+	{
+		eaf_thread_sleep(10);
+	}
+}
+
+static void _monitor_on_load_before(void)
+{
+	eaf_group_local_t* gls = eaf_group_begin();
+	for (g_eaf_monitor_ctx.group.size = 0; gls != NULL; gls = eaf_group_next(gls), g_eaf_monitor_ctx.group.size++)
+	{
+		eaf_service_local_t* sls = eaf_service_begin(gls);
+		for (; sls != NULL; sls = eaf_service_next(gls, sls))
+		{
+			monitor_service_record_t* record = malloc(sizeof(*record));
+			assert(record != NULL);
+
+			record->data.gid = g_eaf_monitor_ctx.group.size;
+			record->data.sid = sls->id;
+			record->data.sls = sls;
+			record->counter.flush_send = 0;
+			record->counter.flush_recv = 0;
+			record->counter.flush_use_time = 0;
+			record->counter.total_recv = 0;
+			record->counter.total_send = 0;
+
+			if (uv_mutex_init(&record->objlock) < 0)
+			{
+				free(record);
+				continue;
+			}
+
+			if (eaf_map_insert(&g_eaf_monitor_ctx.serivce.record_split, &record->node_split) < 0)
+			{
+				free(record);
+				continue;
+			}
+			eaf_map_insert(&g_eaf_monitor_ctx.serivce.record_group, &record->node_group);
+		}
+	}
+
+	g_eaf_monitor_ctx.group.table = malloc(sizeof(monitor_service_record_t) * g_eaf_monitor_ctx.group.size);
+	assert(g_eaf_monitor_ctx.group.table != NULL);
+
+	size_t i;
+	for (i = 0; i < g_eaf_monitor_ctx.group.size; i++)
+	{
+		uv_mutex_init(&g_eaf_monitor_ctx.group.table[i].objlock);
+		g_eaf_monitor_ctx.group.table[i].counter.flush_use_time = 0;
+	}
+}
+
+static void _monitor_on_message_send_after(uint32_t from, uint32_t to, eaf_msg_t* msg, int ret)
+{
+	EAF_SUPPRESS_UNUSED_VARIABLE(msg);
+
+	/* No need to record failure case */
+	if (ret < 0)
+	{
+		return;
+	}
+
+	_monitor_update_message_path(from, to);
+	_monitor_update_counter_send(from);
+}
+
+/**
+ * @brief uv timer callback
+ */
+static void _monitor_on_timer_cb(uv_timer_t* handle)
+{
+	EAF_SUPPRESS_UNUSED_VARIABLE(handle);
+
+	/* We cannot block on this lock because it might affect libuv loop */
+	if (uv_mutex_trylock(&g_eaf_monitor_ctx2.refresh.objlock) < 0)
+	{
+		return;
+	}
+
+	/* reset all necessary fields */
+	_monitor_reset_flush_nolock();
+
+	/* unlock refresh lock */
+	uv_mutex_unlock(&g_eaf_monitor_ctx2.refresh.objlock);
 }
 
 int eaf_monitor_init(unsigned sec)
@@ -400,6 +548,11 @@ int eaf_monitor_init(unsigned sec)
 	if (uv_rwlock_init(&g_eaf_monitor_ctx2.dataflow.rwlock) < 0)
 	{
 		return eaf_errno_unknown;
+	}
+	if (uv_mutex_init(&g_eaf_monitor_ctx2.refresh.objlock) < 0)
+	{
+		ret = eaf_errno_unknown;
+		goto err_init_refresh_lock;
 	}
 
 	g_eaf_monitor_ctx2.refresh.mask.running = 0;
@@ -431,6 +584,8 @@ err_start_timer:
 err_register_hook:
 	uv_close((uv_handle_t*)&g_eaf_monitor_ctx2.refresh.timer, NULL);
 err_init_timer:
+	uv_mutex_destroy(&g_eaf_monitor_ctx2.refresh.objlock);
+err_init_refresh_lock:
 	uv_rwlock_destroy(&g_eaf_monitor_ctx2.dataflow.rwlock);
 	return ret;
 }
@@ -464,32 +619,29 @@ void eaf_monitor_exit(void)
 	}
 
 	uv_rwlock_destroy(&g_eaf_monitor_ctx2.dataflow.rwlock);
+	uv_mutex_destroy(&g_eaf_monitor_ctx2.refresh.objlock);
+
+	size_t i;
+	for (i = 0; i < g_eaf_monitor_ctx.group.size; i++)
+	{
+		uv_mutex_destroy(&g_eaf_monitor_ctx.group.table[i].objlock);
+	}
+	free(g_eaf_monitor_ctx.group.table);
+	g_eaf_monitor_ctx.group.table = NULL;
 }
 
 void eaf_monitor_print_tree(char* buffer, size_t size)
 {
-	buffer[0] = '\0';
+	/**
+	 * The refresh process will reset necessary fields.
+	 *
+	 * For performance consideration, the refresh process is separated into
+	 * multiple steps, which means during refresh process, the report might odd.
+	 *
+	 * So it is better wait for refresh to finish.
+	 */
 
-	APPEND_LINE(buffer, size, "[SERVICE]        [STATE] [RECV]   [SEND]   [S/C]\n");
-
-	uint32_t last_gid = (uint32_t)-1;
-	eaf_map_node_t* it = eaf_map_begin(&g_eaf_monitor_ctx.serivce.record_group);
-	for (; it != NULL; it = eaf_map_next(it))
-	{
-		monitor_service_record_t* record = EAF_CONTAINER_OF(it, monitor_service_record_t, node_group);
-
-		if (last_gid != record->data.gid)
-		{
-			APPEND_LINE(buffer, size, "|- %u\n", (unsigned)record->data.gid);
-			last_gid = record->data.gid;
-		}
-
-		APPEND_LINE(buffer, size, "|  |- %#010"PRIx32" %-7s %-8"PRIu32" %-8"PRIu32" %u/%u\n",
-			record->data.sid,
-			_monitor_state_2_string(record->data.sls->state),
-			record->counter.cnt_recv,
-			record->counter.cnt_send,
-			(unsigned)eaf_message_queue_size(record->data.sls),
-			(unsigned)eaf_message_queue_capacity(record->data.sls));
-	}
+	uv_mutex_lock(&g_eaf_monitor_ctx2.refresh.objlock);
+	_monitor_print_tree_nolock(buffer, size);
+	uv_mutex_unlock(&g_eaf_monitor_ctx2.refresh.objlock);
 }
